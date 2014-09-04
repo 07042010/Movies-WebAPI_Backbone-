1732eed1f11f07f86873d839abbb8c807ce90c09
Denis Chunikhin <denis.chunihin.cleveroad@gmail.com>
2014-09-04 14:50:45 +0300

    Bootstrap decorations added

--- Movies/Scripts/main.js
+++ Movies/Scripts/main.js
@@ -39,7 +39,7 @@
 
     var MovieViews = Backbone.View.extend({
         tagName: 'ul',
-        className: 'unstyled',
+        className: 'nav nav-pills nav-stacked',
         render: function () {
             this.collection.each(this.addOne, this);
             return this;
@@ -50,12 +50,12 @@
         }
     });
 
-    var movId = 0;
+    var movId = 1;
 
     var MovieView = Backbone.View.extend({
         tagName: 'li',
         render: function () {
-            this.$el.html(this.model.get('Id') + '. ' + this.model.get('Name') + ' ( ' + this.model.get('Genre') + ', $' + this.model.get('Cost') + ' )');
+            this.$el.html('<a href="#" >' + this.model.get('Id') + '. ' + this.model.get('Name') + ' ( ' + this.model.get('Genre') + ', $' + this.model.get('Cost') + ' )' + '</a>');
             return this;
         },
         events:{
@@ -63,22 +63,36 @@
         },
         movieClick: function (e) {
             movId = this.model.get('Id');
-            Actors.each(this.updateActors);
-            debugger;
+            updateActors();
+            $('div#actorsblock').empty();
+            actorView.el.innerHTML = ""
             $('div#actorsblock').html(actorView.render().el);
-        },
-        updateActors: function(item, i, arr)
-        {
+            delete_li();
+            $(e.target.parentNode).addClass('active')
+        }
+    });
+
+    function updateActors()
+    {
+        Actors.each(function (item, i, arr) {
             if (item.get('MovieId') == movId)
                 item.set('isVisible', true)
             else
                 item.set('isVisible', false)
-        }
-    });
+        });
+        
+    }
+    function delete_li() {
+        $('li').each(function (i, item) {
+            $(item).removeClass('active');
+            if ($(item).html().indexOf(")") < 0)
+                item.remove();
+        })
+    };
 
     var ActorsViews = Backbone.View.extend({
         tagName: 'ul',
-        className: 'unstyled',
+        className: 'list-group',
         render: function () {
             Actors.each(this.addOne, this);
             return this;
@@ -90,7 +104,8 @@
     });
 
     var ActorView = Backbone.View.extend({
-        tagName: 'li',     	
+        tagName: 'li',
+        className: 'list-group-item',
         template: '#actors',
         render: function () {
             var template = _.template($(this.template).html());
@@ -107,8 +122,10 @@
     Actors.fetch({
         success: function (Actors) {
             console.log(Actors.toJSON());
+            updateActors();
             actorView = new ActorsViews({ collectiovn: Actors });
             $('div#actorsblock').append(actorView.render().el);
+            delete_li();
         }
     });
 
